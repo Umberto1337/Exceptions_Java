7362156078bc9d4e8fc835ad0d5e067e4a8e469a
Alexey <cappuccino13371@gmail.com>
2023-08-06 09:46:42 +0300

    update task2

--- task2.java
+++ task2.java
@@ -1,3 +1,17 @@
-public class task2 {
-    
+class task2 {
+    public static void main(String[] args) {
+        try {
+            int[] intArray = {1, 2, 3, 4, 5, 6, 7};
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching ArithmeticException: " + e);
+            double catchedRes1 = 0.0; // Значение по умолчанию при делении на ноль
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Catching ArrayIndexOutOfBoundsException: " + e);
+        }
+    }
 }
+
